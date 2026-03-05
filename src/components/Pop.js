44a5ac1d8da0f2c5f35dfb8141184d2a29040388
zaif <zaifpasha23422@gmail.com>
2026-03-05 17:14:50 +0530

    enhance Dailogue component with improved layout, styling, and functionality

--- src/components/Pop.js
+++ src/components/Pop.js
@@ -1,29 +1,55 @@
-"use client"
+"use client";
 import Image from "next/image";
 import React from "react";
 import { ImCross } from "react-icons/im";
-const Dailogue = ({list , selected , setSelected}) => {
-  
+import { FaHeart } from "react-icons/fa";
+const Dailogue = ({ list, setSelected }) => {
   if (!list) return null;
   return (
-    <>
-    <div className=" w-[70%]  fixed top-16 left-50 ">
-      <div className="bg-white flex flex-col gap-5 px-5 w-1/2 shadow-2xl mx-auto  ">
-      <button
-        onClick={() => setSelected(!selected)}
-        className="cursor-pointer  absolute right-75 top-10 text-xl"
-              >
-                <ImCross />
-              </button>
-      <Image src={list.Image} height={300} width={300} alt="image"/>
-        <p className="text-red-400 text-2xl">{list.title}</p>
-        <p className="text-slate-400">{list.quantity}</p>
-        <p className="text-slate-400">{list.description}</p>
-        <p className="text-[#009F7F] text-2xl">{list.price}</p>
-        <button className="mx-auto p-4 m-4 bg-[#009F7F] text-white cursor-pointer">Add To Shopping Cart</button>
+    <div className="fixed bg-black/30 top-0 left-0 right-0 bottom-0 z-50
+" onClick={() => setSelected(false)}>
+      <div
+        className="bg-white flex items-center rounded-3xl w-[70%] h-[86%] mx-auto my-25 "
+        onClick={(e) => e.stopPropagation()}
+      >
+        <div className="w-1/2">
+          <Image
+            src={list.Image}
+            height={800}
+            width={800}
+            alt="image"
+          />
+        </div>
+        <div className=" flex flex-col  gap-10 p-8">
+          <p className="flex justify-between items-center text-[#009F7F] text-3xl">
+            {list.title}
+            <span className="border-2 border-slate-200 rounded-3xl p-2 border-slate">
+              <FaHeart />
+            </span>
+          </p>
+          <p className="text-slate-400">{list.quantity}</p>
+          <p className="text-slate-400">{list.description}</p>
+          <p className="text-[#009F7F] text-4xl">{list.price}</p>
+          <div className="flex py-3 items-center gap-8">
+            <button className="bg-[#009F7F] text-white cursor-pointer rounded-xl px-4 py-3 whitespace-nowrap">
+              Add To Shopping Cart
+            </button>
+            <span className="">50 pieces Available</span>
+          </div>
+          <div className="flex gap-4 text-sm items-center">
+            <h1 className="font-bold">Categories</h1>
+            <h2 className="border border-gray-300 p-1">
+              fruits and vegetables
+            </h2>
+            <h2 className="border border-gray-300 p-1">vegetables</h2>
+          </div>
+          <div className="flex gap-4 text-sm items-center">
+            <h1 className="font-bold">Sellers</h1>
+            <h1 className="border border-gray-300 p-1">Grocery Shop</h1>
+          </div>
+        </div>
       </div>
     </div>
-    </>
-  )
+  );
 };
 export default Dailogue;
